68bd9ae8ef55b7b429ab48d4b97c46a081e4ffb2
areebbuzz <areeb22126@gmail.com>
2024-07-20 16:13:27 +0530

    landing page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default ['./index.html'];
+export const theme = {
+  extend: {
+    colors: {
+      'primary': '#3238f2',
+    },
+    fontFamily: {
+      'display': ['Poppins', 'sans-serif'],
+      'body': ['Inter', 'sans-serif']
+    }
+  },
+};
+export const plugins = [];
+
+
